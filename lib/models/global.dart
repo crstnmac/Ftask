f5310b078bbcb2d71ed48c65c539ecccd38690d6
cris <criston2011@gmail.com>
2019-12-17 09:15:41 +0530

    List view

--- lib/models/global.dart
+++ lib/models/global.dart
@@ -3,4 +3,14 @@ import 'package:flutter/material.dart';
 Color darkGreyColor = new Color(0xFF212128);
 Color buttonRedColor = new Color(0xFFDC4F64);
 
-TextStyle intrayTitleStyle = new TextStyle( fontFamily: 'Avanir', fontWeight: FontWeight.bold, fontSize: 40.0, color: darkGreyColor);
+TextStyle intrayTitleStyle = new TextStyle(
+    fontFamily: 'Avanir',
+    fontWeight: FontWeight.bold,
+    fontSize: 40.0,
+    color: darkGreyColor);
+
+TextStyle darkTodoTitle = new TextStyle(
+    fontFamily: 'Avanir',
+    fontWeight: FontWeight.bold,
+    fontSize: 30.0,
+    color: darkGreyColor);
